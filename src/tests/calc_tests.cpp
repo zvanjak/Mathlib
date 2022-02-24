89e94681ee8af8a3e672d6e5e2f751efddc43d5a
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-02-24 01:02:14 +0100

    SREDIO

--- src/tests/calc_tests.cpp
+++ src/tests/calc_tests.cpp
@@ -3,10 +3,19 @@
 #include "../catch/catch.hpp"
 
 #include "calc.h"
+#include "vector.h"
 
 TEST_CASE("Test_Calc", "[simple]") {
     Calculator c;
-	
+
+    Vector a(2);
+
+    a[0] = 1.0;
+    a[1] = 1.0;
+
+    Vector b(2);
+
+    b = 2 * a;
 
 	REQUIRE(4.0 ==  c.Add(2,2));
 }
