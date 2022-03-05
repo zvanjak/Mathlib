d027a76f91ebd1e3797151ee1717a05ac33a5242
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-05 21:52:07 +0100

    idemooo

--- include/MetricTensor.h
+++ include/MetricTensor.h
@@ -1,52 +1,11 @@
-#include "CoordSystems3D.h"
+#include "VectorStatic.h"
 
-template<int N> 
-class Vector
-{
-    public:
-    double  _val[N];
-
-    Vector operator+(Vector &b )
-    {
-        Vector ret;
-        for(int i=0; i<N; i++)
-            ret._val[i] = _val[i] + b._val[i];
-        return ret;
-    }
-
-    Vector operator-(Vector &b )
-    {
-        Vector ret;
-        for(int i=0; i<N; i++)
-            ret._val[i] = _val[i] - b._val[i];
-        return ret;
-    }
-
-};
-
-template<>
-class Vector<3>
-{
-
-};
 
 template<int N, int M> 
 class Matrix
 {
 };
 
-template<int N>
-class CoordTransfNew
-{
-    public:
-    virtual Vector<N> transf(Vector<N> &vec) = 0;
-};
-
-
-class CoordTransfSphericalToCartesian : public CoordTransfNew<3>
-{
-
-};
 
 class MetricTensor3D
 {
