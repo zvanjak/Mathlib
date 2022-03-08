3a6b561640bec9efd8e18b0f9d6c65ec1f2f4152
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-08 21:11:37 +0100

    pomalo

--- include/MatrixStatic.h
+++ include/MatrixStatic.h
@@ -1,8 +1,22 @@
+#include <iostream>
 
 template<int N, int M> 
 class Matrix
 {
     double _vals[N][M];
 
-    double&     operator(int i, int j) { return _vals[i][j]; }
+    public:
+    double&     operator() (int i, int j) { return _vals[i][j]; }
+
+    void Print()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                std::cout << std::setw(10) << std::setprecision(3) << _ptrData[i][j] << ", ";
+            }
+            std::cout << std::endl;
+        }
+    }
 };
--- include/MetricTensor.h
+++ include/MetricTensor.h
@@ -21,16 +21,14 @@ class MetricTensorFromCoordTransf
 
     public:
     MetricTensorFromCoordTransf(CoordSystemTransf<N> &inTransf) : _coordTransf(inTransf)
-    {
-        
-    }
+    { }
 
     virtual double Component(int i, int j, Vector<N> &pos)
     {
         double g_ij = 0.0;
         for(int l=0; l<3; l++)
         {
-            g_ij += Derivation::DerivePartial(_coordTransf.coordTransfFunc(l),i, pos) * Derivation::DerivePartial(_coordTransf.coordTransfFunc(l), j, pos);
+            g_ij += Derivation::DerivePartial(_coordTransf.coordTransfFunc(l), i, pos) * Derivation::DerivePartial(_coordTransf.coordTransfFunc(l), j, pos);
         }
         return g_ij;
     }
@@ -38,6 +36,10 @@ class MetricTensorFromCoordTransf
     virtual Matrix<N, N> MetricAtPoint(Vector<N> &pos)
     {
         Matrix<N, N> ret;
+
+        for( int i=0; i<3; i++ )
+            for( int j=0; j<N; j++ )
+                ret(i,j) = Component(i,j, pos);
 
         return ret;
     }
