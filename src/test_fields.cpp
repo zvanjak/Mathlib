5f10733043ffc9e03d81c978da08be6f9aaf1b8c
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-08 22:06:03 +0100

    idemo do kraja, do 20!!! :))))

--- src/test_fields.cpp
+++ src/test_fields.cpp
@@ -5,7 +5,18 @@ class RadialScalarPotentialCartesian : public ScalarField<3>
     public:
     double Value(Vector<3> &pos)
     {
-        return 0.0;
+        double r = sqrt(pos[0]*pos[0] + pos[1]*pos[1] + pos[2]*pos[2]);
+        return 1/r;
+    }
+
+};
+
+class RadialScalarPotentialSpherical : public ScalarField<3>
+{
+    public:
+    double Value(Vector<3> &pos)
+    {
+        return 1 / pos[0];
     }
 
 };
