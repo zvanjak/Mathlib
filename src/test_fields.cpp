7d9b6e5fdf26159b261d7a24da0e31fd0dc71875
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-08 21:58:26 +0100

    da da

--- src/test_fields.cpp
+++ src/test_fields.cpp
@@ -1,6 +1,16 @@
 #include "Fields.h"
 
+class RadialScalarPotentialCartesian : public ScalarField<3>
+{
+    public:
+    double Value(Vector<3> &pos)
+    {
+        return 0.0;
+    }
+
+};
+
 void Test_Fields()
 {
-    
+
 }
