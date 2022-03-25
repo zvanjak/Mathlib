c455a4d7e1833a4676810551cc7a5f58a76da415
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-25 22:23:43 +0100

    malo

---
+++ include/ElectromagneticTensor.h
@@ -0,0 +1,16 @@
+template<int N, int M>
+class Tensor
+{
+
+};
+
+class EMTensor : public Tensor<4, 4>
+{
+
+};
+
+// single moving charge
+//      bitno je definirati koord.system u kojem se kreće
+//  npr - ide ravno - kartezijan
+//      - ili, ide u  krug, sinhrotron
+// beskonačni vodič
