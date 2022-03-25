9ed00ccb37da6db19b89c2c8cf4a8bd1d4feab56
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-25 22:29:21 +0100

    promišljanja

--- include/Manifold.h
+++ include/Manifold.h
@@ -1,1 +1,12 @@
-class 
+class Manifold
+{
+
+};
+
+/*
+Znači, u svakoj točci ima lokalni kartezije
+NEMA globalnog mapiranja iz nekog skupa koordinata u svaku točku manifolda!
+
+Mobiusov strip!!!
+- ali, n emogu ga deinirati programski bez da ga embedam u 3D!!!
+*/
