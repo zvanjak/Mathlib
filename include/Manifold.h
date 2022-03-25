47611b3305a1a58de2f68c7410416301637fb7ac
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-03-25 22:46:54 +0100

    zanimljive ideje, sad bi tribalo kucati :))))

--- include/Manifold.h
+++ include/Manifold.h
@@ -7,6 +7,11 @@ class Manifold
 Znači, u svakoj točci ima lokalni kartezije
 NEMA globalnog mapiranja iz nekog skupa koordinata u svaku točku manifolda!
 
+Sfera
+- na oba pola definirati sferne .. hmmm?, jel imaju singularitet za r = 0?
+- a za ostatak kartezijeve
+- i onda računati geodesicse
+
 Mobiusov strip!!!
 - ali, n emogu ga deinirati programski bez da ga embedam u 3D!!!
 */
