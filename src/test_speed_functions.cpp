cebbc80d79dd60b5fba5978c7c735ba4933c6660
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-09 00:14:41 +0200

    sad idemo vrijeme paziti

--- src/test_speed_functions.cpp
+++ src/test_speed_functions.cpp
@@ -2,7 +2,7 @@
 
 /*
 FUNKCIJA
-- exp, log, log10, log2, 
+- exp, log, log10, log2,
 - pow, sqrt, cbrt
 - sin, cos, tan, asin, acos, atan
 - sinh, cosh, tanh, asinh, acosh, atanh
@@ -10,17 +10,19 @@ FUNKCIJA
 - Special func - assoc_laguerre, assoc_legendre, beta, comp_ellint_1(2,3), cyl_bessel_I(j,k)
                 - hermite, legendre, lagurre, riemann_zeta, sph_bessel, sph_legendre
 */
-double (*func[])(double) = { sin, cos };
+double (*func[])(double) = {sin, cos};
 
 void Test_Speed_Functions()
 {
-    double x = 0.0;
-    double y = 0.0;
-    for(int i=0; i<10000000; i++)
+    for (int f = 0; f < 2; f++)
     {
-        x = rand() % 1000 * 3.14159 / 500.0;
-        y = sin(x);
-        y = std::hermite(1,x);
+        double x = 0.0;
+        double y = 0.0;
+        for (int i = 0; i < 10000000; i++)
+        {
+            x = rand() % 1000 * 3.14159 / 500.0;
+            y = func[f](x);
+            //y = std::hermite(1, x);
+        }
     }
-
 }
