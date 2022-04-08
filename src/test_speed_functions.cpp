a260b4cb5be066371a2187005e3ebccaf7b0df7d
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-08 23:57:43 +0200

    da da

--- src/test_speed_functions.cpp
+++ src/test_speed_functions.cpp
@@ -14,10 +14,11 @@ void Test_Speed_Functions()
 {
     double x = 0.0;
     double y = 0.0;
-    for(int i=0; i<100000000; i++)
+    for(int i=0; i<10000000; i++)
     {
         x = rand() % 1000 * 3.14159 / 500.0;
         y = sin(x);
+        y = std::hermite(1,x);
     }
 
 }
