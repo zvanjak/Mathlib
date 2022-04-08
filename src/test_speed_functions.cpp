16a05acffb6b80500466d1829c1ccc99f4968bf6
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-08 23:38:06 +0200

    ok

--- src/test_speed_functions.cpp
+++ src/test_speed_functions.cpp
@@ -1,10 +1,10 @@
 #include <cmath>
 
-void Test_Speed_Function()
+void Test_Speed_Functions()
 {
     double x = 0.0;
     double y = 0.0;
-    for(int i=0; i<1000000; i++)
+    for(int i=0; i<100000000; i++)
     {
         x = rand() % 1000 * 3.14159 / 500.0;
         y = sin(x);
