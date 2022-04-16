ca0d6cb8772a6c2324056bdaa70847ee69a9e111
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-16 19:10:49 +0200

    ideje

--- include/physics/BrownMotionSimulator.h
+++ include/physics/BrownMotionSimulator.h
@@ -1,7 +1,13 @@
 class BrownMotionSimulator
 {
 /*
-    - identične čestice sa zadanom masom i randomiziranom brzinom
-    - od N čestica odabrati M, i vidjeti
+    - identične čestice sa zadanom masom i randomiziranom (istom?) brzinom
+    - od N čestica odabrati M, i vidjeti kako izgleda njihov put - da li je prosječno sqrt(n)
+
+    T = 0
+    pomakni svaku česticu za dT
+    svako gibanje generira NDim-1 površinu
+    identificirati sve koje se sijeku
+        za svaku vidjeti da li je stvarno došlo do sudara (možda su se promašili)
 */
 };
